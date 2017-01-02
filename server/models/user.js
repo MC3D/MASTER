b57ea2b719d7e0c95d39ea6eb2071527acd04dce
MC3D <m.chapman.home@gmail.com>
2017-01-02 12:16:27 -0500

    salts password

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// encrypt password On Save Hook
+// run this function before saving a model
+userSchema.pre('save', function(next){
+  // get access to user model
+  const user = this;
+
+  // generate a salt, then run callback
+  bcrypt.genSalt(10, function(err, salt){
+    if (err) {return next(err); }
+
+    // hash password using salt
+    bcrypt.hash(user.passoword, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      // overwirte plain text password with encrypted password
+      user.password = hash;
+      // go ahead and save the model
+      next();
+    });
+  });
 });
 
 // Create the model class; represents all users
