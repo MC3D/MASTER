10531d27f448ee23a6ad50510390b0dfb10c5ece
MC3D <m.chapman.home@gmail.com>
2017-01-02 13:54:54 -0500

    adds passport local strategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,26 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// create local Strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  // verify email and password, call done with user if verified
+  // otherwise, call done with false
+  User.findOne( { email: email }, function(err, user) {
+    if (err) { return done(err); }
+    if(!user) { return done(null, false); }
+
+    // compare passwords ('password' equals user.password)
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    });
+  });
+});
 
 // JWT Strategy options
 const jwtOptions = {
@@ -28,3 +48,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // tell Passport to use this Strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
