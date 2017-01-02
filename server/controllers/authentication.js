85c31e508c127361adac93930f32d03936d729cb
MC3D <m.chapman.home@gmail.com>
2017-01-02 12:02:10 -0500

    requires email and password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -3,6 +3,10 @@ const User = require('../models/user');
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password' });
+  }
   // see if given email exists
   User.findOne({ email: email }, function(err, existingUser) {
     // return error if email exists
