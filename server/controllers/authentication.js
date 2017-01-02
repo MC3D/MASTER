081dce2d69b1fc8dbff71474264ed9cc518fb667
MC3D <m.chapman.home@gmail.com>
2017-01-02 11:42:04 -0500

    checks if user already exists

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,15 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+  // see if given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    
+  });
+  // return error if email exists
+
+  // create and save user if email does not exists
+
+  // respond indicating user was created
 }
