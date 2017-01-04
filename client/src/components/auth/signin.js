02f321b850b12b6ad93accafdd63a0242ad9151c
MC3D <m.chapman.home@gmail.com>
2017-01-04 13:31:44 -0500

    renders signin route

--- client/src/components/auth/signin.js
+++ client/src/components/auth/signin.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 
 class Signin extends Component {
+  _handleSubmit({ email, password }){
+    console.log(email,password);
+  }
+
   render() {
+    const { handleSubmit, fields: { email, password }} = this.props;
+
     return(
-      <form>
+      <form onSubmit={handleSubmit(this._handleSubmit.bind(this))}>
         <fieldset className="form-group">
-          <input placeholder="email" className="form-control" />
+          <input {...email} placeholder="email" className="form-control" />
         </fieldset>
         <fieldset className="form-group">
-          <input placeholder="password" className="form-control" />
+          <input {...password} placeholder="password" className="form-control" />
         </fieldset>
-        <input type="submit" value="Sign In">
+        <input type="submit" value="Sign In" />
       </form>
     );
   }
