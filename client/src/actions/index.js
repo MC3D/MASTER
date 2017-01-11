0331710a4db1c2739e541b56fd78911afed65d74
MC3D <m.chapman.home@gmail.com>
2017-01-11 14:56:14 -0500

    adds redux-thunk

---
+++ client/src/actions/index.js
@@ -0,0 +1,15 @@
+export function signinUser({ email, password}) {
+  return function(dispatch) {
+    // submit email/password to the server
+
+    // if request is good ...
+    // * update state to indicate user is authenticated
+    // * save jwt token
+    // * redirect to router '/feature'
+
+
+    // if request is bad ...
+    // * show error to user
+
+  }
+}
