483bd3e004b4c2cfee047a228498ea8d9df298e9
MC3D <m.chapman.home@gmail.com>
2017-01-11 15:31:46 -0500

    creates signin action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,11 @@
+import axios from 'axios';
+
+const ROOT_URL = "http://localhost:3090";
+
 export function signinUser({ email, password}) {
   return function(dispatch) {
     // submit email/password to the server
-
+    axios.post(`${ROOT_URL}/signin`, { email, password });
     // if request is good ...
     // * update state to indicate user is authenticated
     // * save jwt token
