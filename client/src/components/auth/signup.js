5bfe8d80d75685928690605a17d0228a75cd2ddb
MC3D <m.chapman.home@gmail.com>
2017-01-13 17:01:18 -0500

    continues signup component validation

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -6,18 +6,21 @@ class Signup extends Component {
   render() {
     const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props
 
+    // needs cleanup; DRY
     return (
       <form>
         <fieldset className="form-group">
           <input className="form-control" {...email} placeholder="email" />
-        </fieldset>
+          {email.touched && email.error && <div className="error">{email.error}</div>}
+      </fieldset>
         <fieldset className="form-group">
           <input className="form-control" {...password} type="password" placeholder="password" />
           {password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
         <fieldset className="form-group">
             <input className="form-control" {...passwordConfirm} type="password" placeholder="confirm password" />
-        </fieldset>
+            {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
+      </fieldset>
         <button action="submit" className="btn btn-primary">Register</button>
       </form>
     );
@@ -26,6 +29,19 @@ class Signup extends Component {
 
 function validate(formProps) {
   const errors = {};
+
+  // DRY; cleanup needed
+  if (!formProps.email) {
+    errors.email = "Please enter an email"
+  }
+
+  if (!formProps.password) {
+    errors.password = "Please enter a password"
+  }
+
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = "Please enter a password confirmation";
+  }
 
   if (formProps.password !== formProps.passwordConfirm) {
     errors.password = "Passwords must match"
